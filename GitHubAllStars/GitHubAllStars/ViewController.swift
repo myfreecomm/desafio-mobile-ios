bb6555314876c0bffcaea619a93222df3bfeed49
Raphael Araújo <raphael.araujo@nodo.cc>
2017-10-21 15:32:01 -0400

    Adding initial project structure

---
+++ GitHubAllStars/GitHubAllStars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubAllStars
+//
+//  Created by Raphael Araújo on 21/10/17.
+//  Copyright © 2017 Raphael Araújo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
