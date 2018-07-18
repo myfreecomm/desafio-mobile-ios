0d28688ae003ed0ba143312cd29775d5a655c487
Tiago da Silva Amaral <tiagofly@outlook.com>
2018-07-17 22:38:36 -0300

    Adicionado NetworkHandler para realizar requisições e delegar resposta ao solicitante.

---
+++ source/app/NetworkHandler/NetworkHandler.swift
@@ -0,0 +1,46 @@
+//
+//  NetworkHandler.swift
+//  javahub
+//
+//  Created by Tiago da Silva Amaral on 17/07/18.
+//  Copyright © 2018 com.outlook.tiagofly. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+protocol NetworkInterface{
+
+	func request(
+		_ address: String,
+		operation: HTTPMethod,
+		header: [String: String]?,
+		params: [String: Any]?,
+		completion: @escaping (JSON) -> Void)
+}
+
+class Network: NSObject, NetworkInterface {
+
+	func request(_ address: String, operation: HTTPMethod, header: [String: String]?, params: [String: Any]?, completion: @escaping (JSON) -> Void) {
+
+		UIApplication.shared.isNetworkActivityIndicatorVisible = true
+
+		let urlInstance: URL = URL(string: address)!
+		Alamofire.request(urlInstance, method: operation, parameters: params, encoding: URLEncoding.default, headers: header).responseJSON { (response) in
+
+			UIApplication.shared.isNetworkActivityIndicatorVisible = false
+
+			switch response.result {
+
+			case .success(let value):
+
+				completion(JSON(value))
+
+			case .failure(let error):
+
+				print("Error: \(error)")
+			}
+		}
+	}
+}
