6050f09ed802bcd4bdd8f4a9e2aa4e28228aa737
Tiago da Silva Amaral <tiagofly@outlook.com>
2018-07-17 21:51:50 -0300

    Implementado Router para execução de inicialização de views, e adicionar a stack de RouterViewController.

---
+++ source/app/Router/Router.swift
@@ -0,0 +1,51 @@
+//
+//  Router.swift
+//  javahubTests
+//
+//  Created by Tiago da Silva Amaral on 17/07/18.
+//  Copyright © 2018 com.outlook.tiagofly. All rights reserved.
+//
+
+import UIKit
+
+protocol RouterInterface {
+
+	func goTo(destiny: Routes, pushForward data: Any?)
+}
+
+class Router: NSObject, RouterInterface {
+
+	private weak var view: RouterViewInterface?
+
+	init(view: RouterViewInterface) {
+		super.init()
+
+		self.view = view
+	}
+
+	func goTo(destiny: Routes, pushForward data: Any?) {
+
+		switch destiny {
+
+		case .repositories:
+
+			let viewInstance = buildView(destiny.file, RepositoriesViewController.identifier, RepositoriesViewController.self)
+
+			viewInstance.presenter = Repositories(view: viewInstance, router: self)
+			self.view?.setViewControllers([viewInstance], animated: false)
+
+		case .pullrequest:
+
+			let viewInstance = buildView(destiny.file, PullRequestsViewController.identifier, PullRequestsViewController.self)
+
+			viewInstance.presenter = PullRequests(view: viewInstance)
+			self.view?.setViewControllers([viewInstance], animated: false)
+
+		}
+	}
+
+	private func buildView<T>(_ nameFile: String, _ identifier: String, _ viewClass: T.Type) -> T{
+
+		return UIStoryboard(name: nameFile, bundle: nil).instantiateViewController(withIdentifier: identifier) as! T
+	}
+}
