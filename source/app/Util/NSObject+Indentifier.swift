a199a769fe0a684caa742befcaf8e0f8bf8a373b
Tiago da Silva Amaral <tiagofly@outlook.com>
2018-07-17 21:45:27 -0300

    Implementado extensão para usar o proprio nome da class/tipo para identificação no storyboard

---
+++ source/app/Util/NSObject+Indentifier.swift
@@ -0,0 +1,25 @@
+//
+//  NSObject+Indentifier.swift
+//  javahub
+//
+//  Created by Tiago da Silva Amaral on 17/07/18.
+//  Copyright © 2018 com.outlook.tiagofly. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+protocol Identifier{}
+
+extension Identifier where Self: NSObject{
+
+	static var identifier: String { return String(describing: self) }
+
+	func identifierInstance() -> String{
+
+		return	String(describing: self)
+	}
+}
+
+extension NSObject: Identifier {}
