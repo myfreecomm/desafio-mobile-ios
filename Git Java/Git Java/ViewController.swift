73c9b836c787faa961214286e155006a7828484c
Filipe Amaral Neis <filipe.aneis@gmail.com>
2018-07-25 11:17:36 -0300

    Init Project

---
+++ Git Java/Git Java/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git Java
+//
+//  Created by Filipe Amaral Neis on 25/07/2018.
+//  Copyright © 2018 Neis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
