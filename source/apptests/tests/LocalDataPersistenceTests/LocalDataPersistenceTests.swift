2d1372d4479c24e27b940a13bf4be32a474d9de7
Tiago da Silva Amaral <tiagofly@outlook.com>
2018-07-25 14:52:56 -0300

    Incluido e implementado arquivo para testes de LocalDataPersistence

---
+++ source/apptests/tests/LocalDataPersistenceTests/LocalDataPersistenceTests.swift
@@ -0,0 +1,164 @@
+//
+//  LocalDataPersistenceTests.swift
+//  javahubTests
+//
+//  Created by Tiago da Silva Amaral on 25/07/18.
+//  Copyright © 2018 com.outlook.tiagofly. All rights reserved.
+//
+
+import Quick
+import Nimble
+import RealmSwift
+
+@testable import javahub
+
+class LocalDataPersistenceTests: QuickSpec {
+
+	override func setUp() {
+		super.setUp()
+
+		beforeSuite {
+			Realm.Configuration.defaultConfiguration.inMemoryIdentifier = "RepositoryMock"
+		}
+	}
+
+	override func spec() {
+		
+        describe("Tests LocalDataPersistenceTests") {
+
+			var persistence: LocalDataPersistence!
+
+			context("", closure: {
+
+				beforeEach {
+
+                    // Run before each test
+					persistence = LocalDataPersistence()
+                }
+
+                afterEach{
+
+                     // Run after each test
+					let realm = try! Realm()
+					try! realm.write {
+						realm.deleteAll()
+					}
+                }
+
+                // Puts test code here
+				it("SaveItens", closure: {
+
+					let repository = Repository()
+					repository.identifier = "121512"
+					persistence.saveItens(items: [repository], reNew: false)
+
+					let realm = try!  Realm()
+					let results = realm.objects(Repository.self)
+
+					expect(results.count).to(equal(1))
+					expect(results[0].identifier).to(equal("121512"))
+				})
+
+				it ("updateRepo - clear: TRUE", closure: {
+
+					let repository = Repository()
+					repository.identifier = "121512"
+					let pull = PullRequest()
+					pull.page = 151
+
+					let realm = try! Realm()
+					try! realm.write { repository.pullrequests.append(pull) }
+					try! realm.write { realm.add(repository) }
+
+					let resultA = realm.objects(Repository.self)
+					expect(resultA[0].pullrequests.count).to(equal(1))
+
+					persistence.updateRepo(newPulls: nil, into: repository, clear: true)
+
+					let resultB = realm.objects(Repository.self)
+					expect(resultB[0].pullrequests.count).to(equal(0))
+				})
+
+				it ("updateRepo - clear: FALSE", closure: {
+
+					let repository = Repository()
+					repository.identifier = "121512"
+					let pull = PullRequest()
+					pull.page = 151
+
+					expect(repository.pullrequests.count).to(equal(0))
+
+					persistence.updateRepo(newPulls: [pull], into: repository, clear: false)
+
+					let realm = try! Realm()
+					let resultA = realm.objects(Repository.self)
+					expect(resultA[0].pullrequests.count).to(equal(1))
+
+				})
+
+				it("ClearLocalStorage", closure: {
+
+					var repos = [Repository]()
+
+					for i in 0 ... 10{
+
+						let repo = Repository()
+						repo.identifier = String(i)
+						repos.append(repo)
+					}
+
+					let realm = try! Realm()
+					try! realm.write { realm.add(repos)	}
+
+					let resultA = realm.objects(Repository.self)
+					expect(resultA.count).to(equal(11))
+
+					persistence.clearLocalStorage()
+
+					let resultB = realm.objects(Repository.self)
+					expect(resultB.count).to(equal(0))
+				})
+
+				it("List", closure: {
+
+					var repos = [Repository]()
+
+					for i in 0 ... 10{
+
+						let repo = Repository()
+						repo.identifier = String(i)
+						repo.page = i + 1
+						repo.stars = ((i * 3) + (i * i)) * 325
+						repos.append(repo)
+					}
+
+					let realm = try! Realm()
+					try! realm.write { realm.add(repos)	}
+
+					let results = persistence.list(query: "page > 0", entity: Repository.self, property: "stars", isAcendent: false)
+					expect(results.count).to(equal(11))
+					expect(results[10].stars).to(beLessThan(results[0].stars))
+					expect(results.first?.stars).to(equal(42250))
+				})
+            })
+        }
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
